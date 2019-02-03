a0e95f4a0fbbb0c252b8f8e4d5cf0d6c7a155c7c
Diego Felipe <diegofelipe.14@gmail.com>
2019-02-03 10:04:08 -0400

    commonCharacterCount algorithm

--- arcade/commonCharacterCount.js
+++ arcade/commonCharacterCount.js
@@ -1,25 +1,6 @@
 function commonCharacterCount(s1, s2) {
-	let hash1 = createHashTable(s1.split(''))
-  console.log(hash1)
-	let hash2 = createHashTable(s2.split(''))
-
-    let result = 0
-	Object.keys(hash1).forEach(key => {
-    console.log(hash2[key])
-    hash2[key] ? result++ : result
-    })
-  console.log(result)
-	return result
+    for (var i = 0; i < s1.length; i++) {
+        s2 = s2.replace(s1[i], "!");
+    }
+    return s2.replace(/[^!]/g, "").length;
 }
-
-const createHashTable = arrayToMap => {
-
-	var hashObj = {}
-	arrayToMap.map(word => {
-		if(!hashObj[word]) hashObj[word] = 0
-		hashObj[word]++
-	})
-	return hashObj
-}
-
-commonCharacterCount('asds','a878787')
